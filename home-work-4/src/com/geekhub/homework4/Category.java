221dd708535a67422c3d27f772537472ebd434fe
NatalyaDzygovska <nata.dzygovska@gmail.com>
2016-11-11 13:10:47 +0200

    add Category enum

---
+++ home-work-4/src/com/geekhub/homework4/Category.java
@@ -0,0 +1,7 @@
+package com.geekhub.homework4;
+
+public enum Category {
+    GEEKHUB_TASKS,
+    UNIVERSITY_TASKS,
+    HOUSE_TASKS
+}
--- home-work-4/src/com/geekhub/homework4/Task.java
+++ home-work-4/src/com/geekhub/homework4/Task.java
@@ -2,9 +2,9 @@ package com.geekhub.homework4;
 
 public class Task {
     private String name;
-    private String category;
+    private Category category;
 
-    public Task(String name, String category) {
+    public Task(String name, Category category) {
         this.name = name;
         this.category = category;
     }
@@ -17,11 +17,11 @@ public class Task {
         this.name = name;
     }
 
-    public String getCategory() {
+    public Category getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(Category category) {
         this.category = category;
     }
 }
