78897b58921871281d2cd3838310eea7478f680c
NatalyaDzygovska <nata.dzygovska@gmail.com>
2016-11-12 22:14:00 +0200

    added TaskDateComparator

---
+++ home-work-4/src/com/geekhub/homework4/TaskDateComparator.java
@@ -0,0 +1,11 @@
+package com.geekhub.homework4;
+
+import java.util.Comparator;
+
+public class TaskDateComparator implements Comparator<Task> {
+        @Override
+        public int compare(Task task1, Task task2) {
+            return task1.getDate().compareTo(task2.getDate());
+        }
+
+}
