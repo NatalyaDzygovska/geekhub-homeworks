3c7a0ba2b83a431feef92d688366cc18ced3f5a9
NatalyaDzygovska <nata.dzygovska@gmail.com>
2016-11-20 13:18:05 +0200

    added exception handling in task 1 TranslatorController

--- home-work-5/src/com/geekhub/homework5/task1/TranslatorController.java
+++ home-work-5/src/com/geekhub/homework5/task1/TranslatorController.java
@@ -14,16 +14,18 @@ public class TranslatorController {
 
         Scanner scanner = new Scanner(System.in);
         String command = scanner.next();
-        while(!"exit".equals(command)) {
+        while (!"exit".equals(command)) {
             //TODO: add exception handling here to let user know about it and ask him to enter another path to translation
             //So, the only way to stop the application is to do that manually or type "exit"
+            try {
+                String source = sourceLoader.loadSource(command);
+                String translation = translator.translate(source);
 
-            String source = sourceLoader.loadSource(command);
-            String translation = translator.translate(source);
-
-            System.out.println("Original: " + source);
-            System.out.println("Translation: " + translation);
-
+                System.out.println("Original: " + source);
+                System.out.println("Translation: " + translation);
+            }catch (SourceLoadingException | NullPointerException e){
+                System.out.println("The file by the entered path does not exist. Please, try again: ");
+            }
             command = scanner.next();
         }
     }
